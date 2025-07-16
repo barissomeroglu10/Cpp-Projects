2bab10ab809955f07934b6ce74ccd85b946ef059
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-16 07:34:40 +0300

    Squashed 'AverageOfTenNumbers/' content from commit 51b2248
    git-subtree-dir: AverageOfTenNumbers
    git-subtree-split: 51b2248b6fddc6bef3151b37d0e6807f1a67811c

---
+++ AverageOfTenNumbers/AverageOfTenNumbers.cpp
@@ -0,0 +1,31 @@
+﻿/*
+In this code, we will get ten numbers from the user and then will calculate average for the numbers.
+
+Developer: Barış Someroğlu
+Date: 06.01.2024 / 7:30 p.m.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double Number, Average = 0, Total = 0;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << "Please Enter " << i << ". Number: ";
+		cin >> Number;
+
+		Total += Number;
+	}
+
+	Average = Total / 10;
+
+	cout << "\n";
+
+	cout << "Average is " << Average << endl;
+
+	return 0;
+}
