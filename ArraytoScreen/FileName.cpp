717694f8914b1652509cb865d7d315b06c0aeaa2
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-16 07:04:52 +0300

    Squashed 'ArraytoScreen/' content from commit 261ce81
    git-subtree-dir: ArraytoScreen
    git-subtree-split: 261ce818403210bd6b9569a8c35d14c2346fc44d

---
+++ ArraytoScreen/FileName.cpp
@@ -0,0 +1,21 @@
+/*
+We write our name as a string array and write on the screen
+Developer: Bar?? Somero?lu
+Date: 01.12.23 / 5 p.m.
+*/
+
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	string x[4] = { "My", "Name", "is", "Baris." };
+
+	cout << x[0] << " " << x[1] << " " << x[2] << " " << x[3] << endl;
+
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ArraytoScreen
+We write our name as a string array and write on the screen.
