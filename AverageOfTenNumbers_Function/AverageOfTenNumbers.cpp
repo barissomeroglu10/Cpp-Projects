fa77c70e1646a52a75932d2eec3593ed071b1b06
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-16 07:39:05 +0300

    Squashed 'Average10Number_Function/' content from commit 04f0d3d
    git-subtree-dir: Average10Number_Function
    git-subtree-split: 04f0d3d7160c7ca86d88d32105fd0a2ab70cda98

---
+++ AverageOfTenNumbers_Function/AverageOfTenNumbers.cpp
@@ -0,0 +1,49 @@
+﻿/*
+In this code, we will get 10 numbers from user and calculate average for 10 numbers.
+
+Developer: Barış Someroğlu
+Date: 13.04.2024 - 09:10 pm
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+// Prototype declaration for functions
+void Result(double);
+double Average(double);
+
+int main()
+{
+	double Number;
+	double Total = 0;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << "Please Enter " << i << ". Number = ";
+		cin >> Number;
+
+		Total += Number;
+	}
+
+	// Call the function
+	Result(Total);
+
+	return 0;
+}
+
+// Function declaration
+void Result(double Total)
+{
+	cout << "\n" << "Average of 10 Numbers = " << Average(Total) << endl;
+}
+
+// Function declaration
+double Average(double Total)
+{
+	double Average = Total / 10;
+
+	return Average;
+}
