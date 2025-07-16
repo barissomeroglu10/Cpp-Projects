9cd9c594dcb2495cf980af786aa18daef4d75699
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-16 07:04:44 +0300

    Squashed 'Array_List_Worker/' content from commit e998d13
    git-subtree-dir: Array_List_Worker
    git-subtree-split: e998d13d22858b284edc9a4bb791b5f369da47e4

---
+++ Array_List_Worker/listworkerarray.cpp
@@ -0,0 +1,47 @@
+﻿/*
+In this code we will have five numbers: 1 2 3 4 5
+And then user will enter a number. According to the number, one line list will be showen on the scree.
+
+Developer: Barış Someroğlu
+Date: 12.12.2023 / 10:00 p.m.
+*/
+
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b, c, d, e;
+
+	string W[5] = { "Ceren", "Helin", "Havin", "Baran", "Azadi"};
+
+	a = "Carla, Bebek, Women, 123, 36.5";
+	b = "Harrley, Fenerbahce, Women, 456, 37";
+	c = "Hasky, Kadikoy, Women, 789, 36.5";
+	d = "Bently, Esenyurt, Man, 101, 42";
+	e = "Azadi Kaya, Bagcilar, Man, 202, 43";
+
+	W[0] = a;
+	W[1] = b;
+	W[2] = c;
+	W[3] = d;
+	W[4] = e;
+
+	int Number, x;
+
+	cout << "Please Enter the Number: ";
+	cin >> Number;
+
+	cout << "\n";
+
+	x = Number - 1;
+
+	cout << W[x] << endl;
+
+	cout << "\n";
+
+	return 0;
+}
