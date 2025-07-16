40af79bb8568f2bb0f78768d831358fff2811c69
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-16 07:34:37 +0300

    Squashed 'ArrayToReverse/' content from commit 07c4660
    git-subtree-dir: ArrayToReverse
    git-subtree-split: 07c4660450522e85b56730e4ca8965056b2a5f36

---
+++ ArrayToReverse/ArrayToReverse.cpp
@@ -0,0 +1,49 @@
+﻿/*
+In this code, we will get an array from user (whicl called A). Then we will have "reverse" array (which called B).
+
+Developer: Barış Someroğlu
+Date: 07.01.2024 / 11:50 p.m.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cout << "Please Enter Size of Array: ";
+    cin >> N;
+
+    cout << "\n";
+
+    vector<int> A(N), B(N);
+
+    
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Please Enter " << i + 1 << ". Element: ";
+        cin >> A[i];
+    }
+
+    
+    for (int i = 0; i < N; i++)
+    {
+        B[i] = A[N - 1 - i];
+    }
+
+    cout << "\n";
+
+    cout << "Reverse Array is: ";
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << B[i] << " ";
+    }
+
+    cout << "\n";
+
+    return 0;
+}
